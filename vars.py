583ab99c9cc240f490a89232f0d1e085ede8b717
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-01 12:53:09 +0100

    Add files via upload

--- vars.py
+++ vars.py
@@ -3,13 +3,16 @@ b="#############\n"
 
 
 _v=vars()
+
+
 _a=dict(_v)
+
 counter=0
 for _b in _a:
     if len(_b)>0:
-        _c=_b[0]
         
-        if _c[0:1]!="_":
+        
+        if _b[0:1]!="_":
             print(_b+"#",end="")
             print(_a[_b])
     counter=counter+1
